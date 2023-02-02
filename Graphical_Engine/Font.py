fc6f7edac291266b576d45a5d997f0b85cbee095
Thibault CASTILLON <thibault.castillon@epitech.eu>
2023-02-02 08:01:59 +0100

    [EDIT] removing unessecary methods

--- Graphical_Engine/Font.py
+++ Graphical_Engine/Font.py
@@ -4,8 +4,6 @@ from Graphical_Engine.Colors import get_colors
 class Font():
     def __init__(self, file, size=32):
         pygame.font.init()
-        self.default_color = "black"
-        self.default_bg_color = "white"
         self.default_size = size
         self.file_path = file
         self._font = pygame.font.Font(file, self.default_size)
