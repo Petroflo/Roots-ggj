9d59f130f14392accea35c4bf74661576bdbcf51
Thibault CASTILLON <thibault.castillon@epitech.eu>
2023-02-02 08:03:59 +0100

    [EDIT] main.py -> Actual main.py used for current test

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 import Graphical_Engine.Window as Window
+from Graphical_Engine.Elements.Box import Box
+from Graphical_Engine.Elements.Speech_Box import Speech_Box
+from Graphical_Engine.Font import Font
+from screeninfo import get_monitors
 
 size = (0, 0)
 
-from screeninfo import get_monitors
 for m in get_monitors():
     size = (m.width, m.height)
 
-screen = Window.Window(int(size[0]), int(size[1]), "My Game")
-screen.launch()    
 
-# screen = Window.Window(800, 600, "My Game")
-# print(screen)
+win = Window.Window(int(size[0] * .9), int(size[1] * .9), "My Game")
+font = Font("assets/fonts/Qlassy.ttf", 30)
+box = Box(0, 0, size[0], size[1], win.screen, "white", True)
+speech = Speech_Box(box, font, "Salut, comment vas tu ?")
 
-# screen.launch()
+win.add_element(box)
+win.add_element(speech)
+
+print(win.elements)
+win.launch()
