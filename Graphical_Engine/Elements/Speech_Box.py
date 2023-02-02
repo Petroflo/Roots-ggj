adce16e69a4b68b3c53e9f6758c56e1f7e2fb2b1
Thibault CASTILLON <thibault.castillon@epitech.eu>
2023-02-02 08:03:28 +0100

    [EDIT] SpeechBox needs now : text, Box Element and a font  to be created

--- Graphical_Engine/Elements/Speech_Box.py
+++ Graphical_Engine/Elements/Speech_Box.py
@@ -1,27 +1,26 @@
 import pygame
 import Graphical_Engine.Colors as Colors
 import Graphical_Engine.Elements.Element as Element
+import Graphical_Engine.Elements.Box as Box
 
 class Speech_Box (Element.Element):
-    def __init__(self, text, bg_color, x, y, width, height, screen, fixed):
+    def __init__(self, Box, font, text, bg_color="white", color="black", fixed=False):
         super().__init__()
-        self.text = text
-        self.bg_color = Colors.get_colors(bg_color)
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.screen = screen
-        self.name = "Speech Box :" + text
         self.fixed = fixed
-    
+        self.bg_color = Colors.get_colors(bg_color)
+        self.color = Colors.get_colors(color)
+        self.font = font
+        self.box = Box
+        self.font_object = font.getFont()
+        self.text = self.font_object.render(text, True, self.color, self.bg_color)
+
     def draw(self):
-        pygame.draw.rect(self.screen, self.bg_color, (self.x, self.y, self.width, self.height))
+        rect = self.text.get_rect()
+        rect.move_ip((self.box.width - rect.width) / 2, (self.box.height - rect.height) / 2)
+        self.box.screen.blit(self.text, rect)
         
     def rescale(self, scale):
         if (self.fixed == True):
             return
-        self.x *= scale[0]
-        self.y *= scale[1]
-        self.width *= scale[0]
-        self.height *= scale[1]
+        self.font.rescale(scale)
+
